ef2a99bf8b4f4f1bbeff4018c0b90cee012a21f4
harjap1996 <harjap1996nov@gmail.com>
2025-06-23 23:43:36 -0700

    refactor: added border width and radius

--- src/components/BestScenarioResults/BestScenarioRow.tsx
+++ src/components/BestScenarioResults/BestScenarioRow.tsx
@@ -5,7 +5,7 @@ type BestScenarioRowProps = {
 };
 
 const BestScenarioRow: React.FC<BestScenarioRowProps> = ({ text }) => (
-  <div className="flex justify-between items-center bg-[#CCFF0005] border border-[#C9FF3B] rounded-md p-3 mb-2">
+  <div className="flex justify-between items-center bg-[#CCFF0005] border-[0.5px] border-[#C9FF3B] rounded-[6px] p-3 mb-2">
     <div className="font-sans font-medium text-[16px] leading-[24px] text-[#C9FF3B]">
       {text}
     </div>
