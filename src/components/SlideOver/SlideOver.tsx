170beb1b46c4f6ee48f9e4d1d5fc12f613ef151e
harjap1996 <harjap1996nov@gmail.com>
2025-06-24 14:14:48 -0700

    refactor: fixed the width of the slide over and fixed the background blur

--- src/components/SlideOver/SlideOver.tsx
+++ src/components/SlideOver/SlideOver.tsx
@@ -11,12 +11,12 @@ const SlideOver = ({ isOpen, onClose, children }: SlideOverProps) => {
     <>
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 transition-opacity z-40"
+          className="fixed inset-0 bg-black/50 backdrop-blur-[4px] z-40"
           onClick={onClose}
         />
       )}
       <div
-        className={`fixed top-0 right-0 h-full w-[500px] bg-[#18181A] border-l border-[#525252] shadow-lg z-50 transform transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-full w-[690px] bg-[#18181A] border-l border-[#525252] shadow-lg z-50 transform transition-transform duration-300 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
