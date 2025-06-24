5b5d585fe2dac1d45bddd1d8865af08ead6a7a59
harjap1996 <harjap1996nov@gmail.com>
2025-06-23 20:05:21 -0700

    feat: added custom chart tool tip

---
+++ src/components/Chart/CustomTooltip.tsx
@@ -0,0 +1,34 @@
+import { ChevronUp } from "lucide-react";
+
+const CustomTooltip = ({ active, payload }: any) => {
+  if (!active || !payload || payload.length === 0) {
+    return null;
+  }
+
+  const value = payload[0].value ?? 0;
+  const formattedValue = `$${(value / 1000).toFixed(2)}k`;
+  const percentage = "4.6% above target";
+
+  return (
+    <div className="w-[193px] h-[93px] bg-[#22232433] border border-[#525252] rounded-md p-5 backdrop-blur-[10px]">
+      <div className="flex justify-between items-center mb-3">
+        <div className="text-[20px] font-bold leading-none">
+          {formattedValue}
+        </div>
+        <div className="w-[14px] h-[14px] rounded-full border border-[#888888] flex items-center justify-center text-[#888888] text-xs">
+          ?
+        </div>
+      </div>
+      <div className="flex items-center gap-[10px]">
+        <div className="w-[16px] h-[16px] rounded-full border border-[#C8E972] p-[3px] flex items-center justify-center">
+          <ChevronUp size={10} className="text-[#C8E972]" />
+        </div>
+        <div className="text-[14px] font-normal text-[#878787] leading-none">
+          {percentage}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomTooltip;
