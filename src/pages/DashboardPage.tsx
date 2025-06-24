c0d005e1b752e8f5b14b01a973e5cfc166905997
harjap1996 <harjap1996nov@gmail.com>
2025-06-23 23:47:46 -0700

    refactor: added KeyIndicatorsPanel to dashboard and also rearranged component layout

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,13 +1,24 @@
 import Layout from "../components/Layout/Layout";
 import Chart from "../components/Chart/Chart";
 import BestScenarioResults from "../components/BestScenarioResults/BestScenarioResults";
+import KeyIndicatorsPanel from "../components/KeyIndicatorsPanel/KeyIndicatorsPanel";
 
 const DashboardPage = () => (
   <Layout>
     <div className="flex flex-col gap-6">
       <BestScenarioResults />
-      <h2 className="text-xl font-semibold">Graphs</h2>
-      <Chart />
+      <div className="flex gap-6 items-start">
+        <div>
+          <h2 className="text-xl font-semibold pb-[15px]">Graphs</h2>
+          <Chart />
+        </div>
+        <div>
+          <h2 className="text-xl font-semibold pb-[15px]">
+            Key Performance Indicators
+          </h2>
+          <KeyIndicatorsPanel />
+        </div>
+      </div>
     </div>
   </Layout>
 );
