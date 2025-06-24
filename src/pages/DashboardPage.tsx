56e1c6530c3b0798ba315eccfeafd626066edced
harjap1996 <harjap1996nov@gmail.com>
2025-06-23 20:53:26 -0700

    refactor: added best results to dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,11 @@
 import Layout from "../components/Layout/Layout";
 import Chart from "../components/Chart/Chart";
+import BestScenarioResults from "../components/BestScenarioResults/BestScenarioResults";
 
 const DashboardPage = () => (
   <Layout>
     <div className="flex flex-col gap-6">
+      <BestScenarioResults />
       <h2 className="text-xl font-semibold">Graphs</h2>
       <Chart />
     </div>
